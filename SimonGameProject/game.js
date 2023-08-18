8e6db271b14b9b6f00c76e4b62b3f8b5e5b52712
txellext <txellmgsol@gmail.com>
2023-08-18 18:47:32 +0200

    Feat: title changed once game started

--- SimonGameProject/game.js
+++ SimonGameProject/game.js
@@ -1,9 +1,31 @@
 // console.log($("h1"));
 var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
 var userClickedPattern = [];
+var started = false;
+var level = 0;
 
-function nextSequence(){
+
+$(document).keypress(function() {
+    if (!started) {
+        $("h1").text("Level" + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+
+$(".btn").click(function() {
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+
+function nextSequence() {
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
    
@@ -12,17 +34,23 @@ function nextSequence(){
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
-    audio.play();
-
-
-    var userChosenColour = $(".btn").on("click", function (e){
-        alert("Handler for 'click'" + e);
-        console.log(e.classList(randomChosenColour));
-    })
-
-    userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    playSound(randomChosenColour);
+    
+    level++;
+    $("h1").text("Level " + level);
 }
 
-$(document).keypress(nextSequence);
+
+function playSound(name) {
+    var audio = new Audio("./sounds/" + name + ".mp3");
+    audio.play();
+};
+
+
+function animatePress(currentColour) {
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function () {
+        $("#"+currentColour).removeClass("pressed")
+        }, 100)
+};
+
